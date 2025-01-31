42cf54677afd715691f07d6df4e4882aefbc3473
nlobachev <nikita.lobachev@fintech-farm.com>
2025-01-31 11:25:36 +0200

    add basic converter

--- src/app/Main.java
+++ src/app/Main.java
@@ -5,21 +5,14 @@ public class Main {
     private static final double CONV_K = 2.20462;
 
     public static void main(String[] args) {
-        System.out.println("App for measures converting.");
-        System.out.println("Version 1.0.");
+        System.out.println("Converter App.");
+
         double kgs = 5;
-        double pnds = 12;
         double pounds = convKgsToPounds(kgs);
-        double kilos = convPoundsToKgs(pnds);
-        System.out.println("Result is " + pounds + " pounds and "
-                + kilos + " kgs.");
+        System.out.println("Result is " + pounds + " pounds.");
     }
 
     private static double convKgsToPounds(double kgs) {
         return kgs * CONV_K;
-    }
-
-    private static double convPoundsToKgs(double pnds) {
-        return pnds / CONV_K;
     }
 }
